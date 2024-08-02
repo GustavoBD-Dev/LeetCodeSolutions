5d30f16a8810738db945f6bc9bf31e41406e0bac
Gustavo Blas Duran <gustavoblasduran@gmail.com>
2024-08-01 23:05:22 -0600

    Add solution

--- LeetCode 75/Greatest Common Divisor of Strings.py
+++ LeetCode 75/Greatest Common Divisor of Strings.py
@@ -29,3 +29,16 @@ str1 and str2 consist of English uppercase letters.
 
 """
 
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+       
+        if str1 + str2 != str2 + str1:
+            return ""
+        
+        def gcd(a, b):
+            while b:
+                a, b = b, a % b
+            return a
+            
+        return str1[:gcd(len(str1), len(str2))]
+
